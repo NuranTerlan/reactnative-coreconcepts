ec17c13b4f6f3cafecb2c466343c7462036b8306
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-13 03:53:41 +0400

    Create Card.js

---
+++ app/components/Card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import colors from "../config/colors";
+
+import { AppText } from "../components/Texts";
+
+function Card({ title, subTitle, image }) {
+	return (
+		<View style={styles.card}>
+			<Image style={styles.image} source={image} />
+			<View style={styles.details}>
+				<AppText style={styles.title}>{title}</AppText>
+				<AppText style={styles.subTitle}>{subTitle}</AppText>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	card: {
+		borderRadius: 15,
+		backgroundColor: colors.white,
+		marginBottom: 20,
+		overflow: "hidden",
+		elevation: 35,
+	},
+	image: {
+		width: "100%",
+		height: 200,
+	},
+	details: {
+		padding: 20,
+	},
+	title: {
+		marginBottom: 7,
+	},
+	subTitle: {
+		color: colors.secondary,
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+});
+
+export default Card;
