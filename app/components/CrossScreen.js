559f35623b3745e7474066cf48135dcb7bf3f958
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-13 19:50:29 +0400

    Update CrossScreen.js

--- app/components/CrossScreen.js
+++ app/components/CrossScreen.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import Constants from "expo-constants";
 
-function CrossScreen({ children }) {
-	return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+function CrossScreen({ children, bg = "#fff", style }) {
+	return (
+		<SafeAreaView
+			style={[
+				{
+					backgroundColor: bg,
+				},
+				styles.screen,
+			]}
+		>
+			<View style={[{ ...style }]}>{children}</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
