2913ccc6fb68724e3e03a95c9d0159440662c7c1
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-18 02:52:24 +0400

    Update RegisterButton.js

--- app/components/Buttons/AuthButtons/RegisterButton.js
+++ app/components/Buttons/AuthButtons/RegisterButton.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { View } from "react-native";
+import { useFormikContext } from "formik";
 
 import colors from "../../../config/colors";
 import AuthButton from "./AuthButton";
 
-function RegisterButton({ style }) {
+function RegisterButton({ action, style }) {
+	const { values } = useFormikContext();
+
 	return (
 		<View style={style}>
 			<AuthButton
-				action={() => console.log("Registration")}
+				action={
+					action
+						? action
+						: () => {
+								for (let prop in values) {
+									console.log(`${prop}: ${values[prop]}`);
+								}
+						  }
+				}
 				type="Register"
-				bg={colors.secondary}
+				bg={colors.primary}
 			/>
 		</View>
 	);
