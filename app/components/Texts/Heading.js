95671bc771001264923f2d406e1ccf23befcf06d
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-17 04:35:48 +0400

    Update Heading.js

--- app/components/Texts/Heading.js
+++ app/components/Texts/Heading.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+
+import { AppTextStyles } from "../../config/styles";
 
 import AppText from "./AppText";
 
 function Heading({ children, style }) {
-	return <AppText style={[styles.text, { ...style }]}>{children}</AppText>;
+	return (
+		<AppText style={[AppTextStyles.heading, { ...style }]}>{children}</AppText>
+	);
 }
-
-const styles = StyleSheet.create({
-	text: {
-		fontSize: 22,
-		fontWeight: "bold",
-		color: "#303841",
-	},
-});
 
 export default Heading;
