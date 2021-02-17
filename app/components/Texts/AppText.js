d69604a081b6099bad93c8b43951a535459a8b2a
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-17 04:35:45 +0400

    Update AppText.js

--- app/components/Texts/AppText.js
+++ app/components/Texts/AppText.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text, StyleSheet } from "react-native";
+
+import { AppTextStyles } from "../../config/styles";
 
 function AppText({ children, style }) {
-	return <Text style={[styles.text, { ...style }]}>{children}</Text>;
+	return (
+		<Text style={[AppTextStyles.default, styles.text, { ...style }]}>
+			{children}
+		</Text>
+	);
 }
 
 const styles = StyleSheet.create({
 	text: {
-		fontFamily: Platform.OS === "android" ? "Roboto" : "San Francisco",
-		fontSize: 16,
 		fontWeight: "600",
 	},
 });
