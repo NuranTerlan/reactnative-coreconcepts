b6b4b9b25a71ee2855fbe76351c4070b8c2f83ac
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-17 04:35:23 +0400

    Create AppTextInput.js

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import { AppTextStyles } from "../config/styles";
+
+function AppTextInput({ icon, ...rest }) {
+	return (
+		<View style={styles.container}>
+			{icon && (
+				<MaterialCommunityIcons
+					name={icon}
+					size={20}
+					color={colors.medium}
+					style={styles.icon}
+				/>
+			)}
+			<TextInput
+				defaultValue=""
+				{...rest}
+				style={[AppTextStyles.default, styles.input]}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: colors.light,
+		borderRadius: 99,
+		flexDirection: "row",
+		alignItems: "center",
+		width: "100%",
+		paddingVertical: 15,
+		paddingHorizontal: 22,
+		marginVertical: 10,
+		overflow: "hidden",
+	},
+	icon: {
+		marginRight: 10,
+	},
+	input: {
+		flex: 1,
+	},
+});
+
+export default AppTextInput;
