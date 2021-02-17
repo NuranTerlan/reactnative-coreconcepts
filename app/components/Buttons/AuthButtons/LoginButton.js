3f557c2fb9d16c26d2d870c70c73105f593537fd
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-18 02:52:22 +0400

    Update LoginButton.js

--- app/components/Buttons/AuthButtons/LoginButton.js
+++ app/components/Buttons/AuthButtons/LoginButton.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { View } from "react-native";
+import { useFormikContext } from "formik";
 
 import colors from "../../../config/colors";
 import AuthButton from "./AuthButton";
 
 function LoginButton({ action, style }) {
+	const { values } = useFormikContext();
+
 	return (
 		<View style={style}>
 			<AuthButton
-				action={action ? action : () => console.log("Login")}
+				action={
+					action
+						? action
+						: () => {
+								for (let prop in values) {
+									console.log(`${prop}: ${values[prop]}`);
+								}
+						  }
+				}
 				type="Login"
 				bg={colors.primary}
 			/>
